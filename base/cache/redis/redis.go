583fd6d34377a6956c4d70a157f0c662b0cd182c
imoh chinedu <respectedfxinfo@gmail.com>
2024-07-16 21:11:18 +0100

    testing 2

--- base/cache/redis/redis.go
+++ base/cache/redis/redis.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"loveair/base/cache"
 	"time"
 
@@ -26,6 +27,7 @@ func InitRedisConnection(cacheConfig map[string]string) cache.Interface {
 		Username:   cacheConfig["remote_username"], // no username set
 		Password:   cacheConfig["remote_password"], // no password set
 		DB:         0,                              // Redis has 16 logical db's in an instance.
+		OnConnect:  func(ctx context.Context, cn *redis.Conn) error { fmt.Println("Redis connected..."); return nil },
 		MaxRetries: 9000})
 
 	remoteRJH := rejson.NewReJSONHandler()
@@ -37,7 +39,8 @@ func InitRedisConnection(cacheConfig map[string]string) cache.Interface {
 		Addr:       cacheConfig["local_url"],
 		Username:   cacheConfig["local_username"], // no username set
 		Password:   cacheConfig["local_password"], // no password set
-		DB:         1,                             // Redis has 16 logical db's in an instance.
+		DB:         1,
+		OnConnect:  func(ctx context.Context, cn *redis.Conn) error { fmt.Println("Redis connected..."); return nil }, // Redis has 16 logical db's in an instance.
 		MaxRetries: 9000})
 
 	// Cache email pin data.
@@ -47,6 +50,7 @@ func InitRedisConnection(cacheConfig map[string]string) cache.Interface {
 		Username:   cacheConfig["local_username"], // no username set
 		Password:   cacheConfig["local_password"], // no password set
 		DB:         2,                             // Redis has 16 logical db's in an instance.
+		OnConnect:  func(ctx context.Context, cn *redis.Conn) error { fmt.Println("Redis connected..."); return nil },
 		MaxRetries: 9000})
 
 	return &Redis{
